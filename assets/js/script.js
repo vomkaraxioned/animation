ae09918b268abbe3ab5181937849e6c75411719c
vomkaraxioned <omkarv@axioned.com>
2022-08-08 16:51:51 +0530

    js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
 /* Author: 
 
 */
+let box = $(".box");
+
+
+function animation() {
+    box.animate({ width: "100%" }, "slow", function() {
+        box.css("background-color", "#ff0");
+    });
+    box.animate({ left: '80%', width: '200px' }, "slow", function() {
+        box.css("background-color", "#f0f");
+    });
+    box.animate({ left: '0', width: '100%' }, "slow", function() {
+        box.css("background-color", "#0ff");
+    });
+    box.animate({ width: '200px' }, "slow", function() {
+        box.css("background-color", "#ff4500");
+    });
+}
+
 $(document).ready(function() {
-    $(".box").click(function() {
-        $(this).animate({
-            width: '100%',
-            right: '0',
-            width: '200px',
-        }, "slow");
+    box.click(function() {
+        setInterval(animation, 200);
     });
 });
